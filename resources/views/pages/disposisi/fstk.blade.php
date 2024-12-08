dde900ad0ec20914b94d777f90383235d8ddfda8
Zainal Saputra <101336276+zainalsaputra@users.noreply.github.com>
2024-12-08 12:24:39 +0700

    update disposisi title

--- resources/views/pages/disposisi/fstk.blade.php
+++ resources/views/pages/disposisi/fstk.blade.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TUUD | KESDAM IV/DIPONEGORO</title>
+    <title>TUUD | DENKESYAH 18.04.01 SORONG</title>
     <link rel="shortcut icon" href="{{ asset('static/favicon.ico') }}">
     <link rel="stylesheet" href="{{ asset('css/disposisi/fstk.css') }}">
 </head>
